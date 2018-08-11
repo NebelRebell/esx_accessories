8fb8146e76b113d791ea8398499ae1d75ec647c3
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2018-08-11 16:39:29 +0200

    German SQL
    Includes de_esx_accessories.sql in localization

---
+++ localization/de_esx_accessories.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `datastore` (name, label, shared) VALUES
+  ('user_ears','Ohrschmuck',0),
+  ('user_glasses','Brillen',0),
+  ('user_helmet','Helme und Hüte',0),
+  ('user_mask','Maske',0)
+;
